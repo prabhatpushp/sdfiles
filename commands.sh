de78f0c7b8eb87d81f63ae16c86d69d148beb23f
prabhat pushp <46195662+prabhatpushp@users.noreply.github.com>
2023-06-09 18:32:01 +0530

    Update commands.sh

--- commands.sh
+++ commands.sh
@@ -1,2 +1,3 @@
 echo "Commands.sh"
 wget https://raw.githubusercontent.com/prabhatpushp/dump/main/a.jpeg -P /MyFiles/stable-diffusion-ui/
+wget https://raw.githubusercontent.com/prabhatpushp/lkounivcovid19/master/stats.html -P /MyFiles/stable-diffusion-ui/
